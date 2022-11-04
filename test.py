a0825efbbea63ab67ac0d8826bb1e5326d1a38c2
abrahamjimenez <abraham.jimenez.romo@gmail.com>
2022-11-03 19:51:47 -0600

    added table

--- test.py
+++ test.py
@@ -1,1 +1,10 @@
-print("hi")
+def main():
+    print("1 | 2 | 3\n- + - + -\n4 | 5 | 6\n- + - + -\n7 | 8 | 9")
+
+    playerXTurn()
+
+def playerXTurn():
+    input("x's turn to choose a square (1-9): ")
+
+if __name__ == "__main__":
+    main()
